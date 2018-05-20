212826e3c1f2d2845e51817504d48ee80a0e5318
Adam Grigolato <adam.grigolato@balcora.net>
2018-05-20 11:01:00 +1000

    Shifting internal structure about to be better flowing for development on multiple modules

---
+++ src/rollout-base/lib/index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Imports
+
+
+
+
+
+# Functions
+
+
+
+
+
+
+
+
+
+
+
+# Classes
+
+class Index(object):
+    def __init__(self):
+        self.indexid = generate_id()
+        self.tophash = none
+        self.s4id = none
+        self.s4parent = none
---
+++ src/rollout-host/lib/disk.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# IMPORTS
+
+
+
+
+
+
+
+
+
+## Functions
+# 
+
+def get_disk_geometry():
+
+
+def get_partition_table():
+
+
+
